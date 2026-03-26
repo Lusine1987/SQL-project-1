dc1e499d1d758a703d72f9270f2f91f8f5a8fbc3
Lusine1987 <167713892+Lusine1987@users.noreply.github.com>
2026-03-27 02:04:37 +0300

    Add files via upload

---
+++ day00_ex02.sql
@@ -0,0 +1,11 @@
+-- первый вариант операторы сравнения (<=, >=)
+SELECT name, rating
+FROM pizzeria
+WHERE rating >= 3.5 AND rating <= 5
+ORDER BY rating;
+
+-- второй вариант ключевое слово BETWEEN
+SELECT name, rating
+FROM pizzeria
+WHERE rating BETWEEN 3.5 AND 5
+ORDER BY rating;
